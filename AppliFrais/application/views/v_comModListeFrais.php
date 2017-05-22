ad992b20a369841ab772af03bbdeca8cf1783ed3
le_loeuff <le_loeuff@B4-W7-11.sio.chaptalqper>
2017-05-22 13:32:45 +0200

    Version finale n°1.

---
+++ AppliFrais/application/views/v_comModListeFrais.php
@@ -0,0 +1,69 @@
+<?php
+$this->load->helper('url');
+?>
+
+<div id="contenu">
+	<h2>Modifier la fiche de frais du mois <?php echo $numMois."-".$numAnnee; ?> | Identifiant visiteur: <?php echo $idVisiteur ?></h2>
+					
+	<?php if(!empty($notify)) echo '<p id="notify" >'.$notify.'</p>';?>
+	 
+	<form method="post"  action="<?php echo base_url("c_comptable/majForfait/".$numAnnee.$numMois.'/'.$idVisiteur);?>">
+		<div class="corpsForm">
+			<fieldset>
+				<legend>Eléments forfaitisés</legend>
+				<table>
+				<tr>
+				<th></th>
+				<th>Quantite</th>
+				<th>Montant</th>
+				<th>Total</th>
+				</tr>
+				<?php
+					foreach ($lesFraisForfait as $unFrais)
+					{
+						$idFrais = $unFrais['idfrais'];
+						$libelle = $unFrais['libelle'];
+						$quantite = $unFrais['quantite'];
+						$montant = $unFrais['montant'];	
+						$fonction = 'calculTotal('.$idFrais.')';
+						
+						echo 
+						'
+						<tr>
+							<td>
+								<label for="'.$idFrais.'">'.$libelle.'</label>
+							</td>
+							<td>
+								<input name="lesFrais['.$idFrais.']" disabled="disabled" type="text" id="'.$idFrais.'" size="10" maxlength="5" value="'.$quantite.'" />
+							</td>
+							<td>
+								<input name="lesMontants['.$idFrais.']" required="required" type="text" id="montant'.$idFrais.'" size="10" maxlength="5" value="'.$montant.'" onchange="calculTotal('.$idFrais.')"/> 
+							</td>
+							<td>
+								<label id="total'.$idFrais.'" for="'.$idFrais.'">0</label>
+							</td>
+						</tr>
+						';
+					}
+				?>
+				<tr>
+				<td></td>
+				<td></td>
+				<td><b>Total forfaitisés :</b></td>
+				<td><label id="totalfinal">0</label></td>
+				</tr>
+				</table>
+				</br>
+				<div id="erreurSaisi"></div>
+			</fieldset>
+		</div>
+		<div class="piedForm">
+			<p>
+				<input id="ok" type="submit" value="Enregistrer" size="20" onclick="verifCaractereAutorise()" />
+				<input id="annuler" type="reset" value="Effacer" size="20" />
+			</p> 
+		</div>
+	</form>
+
+
+</div>
